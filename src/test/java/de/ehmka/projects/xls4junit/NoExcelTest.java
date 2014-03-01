2991d1315d1ff3f5a74a9ccf1505e07503925aea
Michael Krausse <michael.krausse@m-krausse.de>
2014-03-01 15:21:23 +0100

    projekt created with one dummy test

---
+++ src/test/java/de/ehmka/projects/xls4junit/NoExcelTest.java
@@ -0,0 +1,15 @@
+package de.ehmka.projects.xls4junit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by michi on 01/03/14.
+ */
+public class NoExcelTest {
+
+    @Test
+    public void testNichts() {
+        Assert.assertTrue("Beispiel fuer einen Test ohne Excel.", true);
+    }
+}
